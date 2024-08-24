4b2f4e9b08643972acb5bd12b6838914c176e255
yogendrabaskota <yogendrabaskota18@gmail.com>
2024-08-24 23:09:57 +0545

    ok

--- app.js
+++ app.js
@@ -10,8 +10,6 @@ app.get("/home",(req,res)=>{
     })  
 })
 
-
-
 app.patch("/home/:id",(req,res)=>{
     res.status(200).json({
         message : "successfuly showing"
@@ -22,12 +20,11 @@ app.get("/home/:id",(req,res)=>{
         message : "successfuly showing"
     })
 })
-app.post("/home/:id",(req,res)=>{
+apsp.post("/home/:id",(req,res)=>{
     res.status(200).json({
         message : "successfuly showing"
     })  
 })
-
 app.listen(3000,()=>{
     console.log("successfulLy")
 })
