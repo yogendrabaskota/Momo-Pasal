b425801b05d633b2003d1f6d30781e10940d1974
yogendrabaskota <yogendrabaskota18@gmail.com>
2024-08-25 23:42:21 +0545

    minor change

--- app.js
+++ app.js
@@ -3,13 +3,11 @@ const mongoose = requrie('mongoose')
 
 app.get("/",(req,res)=>{  
 })
-
 app.get("/home",(req,res)=>{
     res.status(200).json({
         message : "successfuly showing"
     })  
 })
-
 app.patch("/home/:id",(req,res)=>{
     res.status(200).json({
         message : "successfuly showing"
@@ -20,7 +18,8 @@ app.get("/home/:id",(req,res)=>{
         message : "successfuly showing"
     })
 })
-apsp.post("/home/:id",(req,res)=>{
+
+app.post("/home/:id",(req,res)=>{
     res.status(200).json({
         message : "successfuly showing"
     })  
