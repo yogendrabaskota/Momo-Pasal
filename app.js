018db5133aaf9bebfbd5ddf573e43ee3785b9813
yogendrabaskota <yogendrabaskota18@gmail.com>
2024-09-24 16:34:05 +0545

    mvcr pattern implemented

--- app.js
+++ app.js
@@ -1,13 +1,15 @@
 const express = require('express')
 const { connectDatabase } = require('./Database/database')
-const User = require('./model/userModel')
-
 const app = express()
 
 // tell node to use dotenv
 require("dotenv").config()
-const bcrypt = require("bcryptjs")
-const jwt = require("jsonwebtoken")
+
+//Routes 
+const authRoute = require("./routes/authRoute")
+
+//end routes
+
 
 
 app.use(express.json())
@@ -23,75 +25,14 @@ app.get("/",(req,res)=>{
 })
 
 
-//register user api
-app.post("/register",async(req,res)=>{
-     const {email,password,phoneNumber,username} = req.body
-     if(!email || !password || !phoneNumber || !username) {
-        res.status(400).json({
-            message : "please provide email,password,phoneNumber"
-        })
-    }
-   // check that if email user already exist or not 
-   const userFound = await User.find({userEmail : email})
-   if(userFound.length > 0) {
-    return res.status(400).json({
-        message : "user with that email already exist. please use unique email"
-    })
-   }
+app.use("",authRoute)
 
-   
-   //creating database collection
-    await User.create({ 
-        userName : username,
-        userPhoneNumber : phoneNumber,
-        userEmail : email,
-        userPassword : bcrypt.hashSync(password,10)
-    })
-    res.status(201).json({
-        message : "User registered successfully"
-    })
-    
-})
+//register user api
+//app.post("/register",registerUser)
 
 //login user api
-app.post("/login",async(req,res)=>{
-    const{email, password} = req.body
-    if(!email || !password) {
-        return res.status(400).json({
-            message : "please provide email and password"
-        })
-    }
-    //check if user with that email exists or not
-    const userFound = await User.find({userEmail : email})
-    if(userFound.length == 0) {
-        return res.status(404).json({
-            message : "User with that email is not registered"
-        })
-    }
-    //password check
-    const isMatched = bcrypt.compareSync(password,userFound[0].userPassword)
-    if(isMatched) {
-        // generate token
-        const token = jwt.sign({id : userFound[0]._id},process.env.SECRET_KEY,{
-        expiresIn : '30d'
-        })
-        
-        
+//app.post("/login",loginUser)
 
-
-        res.status(200).json({
-            message : " User logged in successfully ",
-            token
-        })
-    }else{
-        res.status(404).json({
-            message : "invalid password"
-    
-        }) 
-    }
-
-
-})
 
 
 // listen server
---
+++ controller/auth/authController.js
@@ -0,0 +1,72 @@
+
+
+const bcrypt = require("bcryptjs")
+const jwt = require("jsonwebtoken")
+const User = require("../../model/userModel")
+
+
+exports.registerUser = async(req,res)=>{
+    const {email,password,phoneNumber,username} = req.body
+    if(!email || !password || !phoneNumber || !username) {
+       res.status(400).json({
+           message : "please provide email,password,phoneNumber"
+       })
+   }
+  // check that if email user already exist or not 
+  const userFound = await User.find({userEmail : email})
+  if(userFound.length > 0) {
+   return res.status(400).json({
+       message : "user with that email already exist. please use unique email"
+   })
+  }
+
+  
+  //creating database collection
+   await User.create({ 
+       userName : username,
+       userPhoneNumber : phoneNumber,
+       userEmail : email,
+       userPassword : bcrypt.hashSync(password,10)
+   })
+   res.status(201).json({
+       message : "User registered successfully"
+   })
+   
+}
+
+exports.loginUser = async(req,res)=>{
+    const{email, password} = req.body
+    if(!email || !password) {
+        return res.status(400).json({
+            message : "please provide email and password"
+        })
+    }
+    //check if user with that email exists or not
+    const userFound = await User.find({userEmail : email})
+    if(userFound.length == 0) {
+        return res.status(404).json({
+            message : "User with that email is not registered"
+        })
+    }
+    //password check
+    const isMatched = bcrypt.compareSync(password,userFound[0].userPassword)
+    if(isMatched) {
+        // generate token
+        const token = jwt.sign({id : userFound[0]._id},process.env.SECRET_KEY,{
+        expiresIn : '30d'
+        })
+        
+        
+
+
+        res.status(200).json({
+            message : " User logged in successfully ",
+            token
+        })
+    }else{
+        res.status(404).json({
+            message : "invalid password"
+    
+        }) 
+    }
+}
