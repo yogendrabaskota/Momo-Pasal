cdd61a7af3abae4314e48aa5119cd2f40ddaff8a
yogendrabaskota <yogendrabaskota18@gmail.com>
2024-09-26 15:48:38 +0545

    admin seeded

--- Database/database.js
+++ Database/database.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose")
+const User = require("../model/userModel")
 
 
 exports.connectDatabase = async()=>{
@@ -9,4 +10,27 @@ await mongoose.connect(process.env.MONGO_URI)
 // await =  wait till connected to databases
 console.log("database connected successfully")
     
+
+// admin seeding
+
+//check whether the admin exists or not 
+const isAdminExists = await User.findOne({userEmail : "admin@gmail.com"})
+if(!isAdminExists){
+    await User.create({
+        userEmail : "admin@gmail.com",
+        userPassword : "admin",
+        userPhoneNumber : "9090",
+        userName : "admin",
+        role : "admin"
+    })
+    console.log("Admin seeded successfully")
+
+}else{
+    console.log("Admin already seeded")
+}
+
+
+
+
+
 }
