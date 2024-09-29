8ffbee19f4b298d3f1fc400b49a1ab03f30e3cc7
yogendrabaskota <yogendrabaskota18@gmail.com>
2024-09-29 21:25:51 +0545

    ok

--- middleware/isAuthenticated.js
+++ middleware/isAuthenticated.js
@@ -1,6 +1,8 @@
-const isAuthenticated = (req,res,next) => {
-    
+const jwt = require("jsonwebtoken")
+const { promisify } = require("utils").promisify
 
+
+const isAuthenticated = async (req,res,next) => {
 const token = req.headers.authorization
 if(!token) {
     res.status(403).json({
@@ -8,17 +10,22 @@ if(!token) {
     })
 }
 //verify token is legit or not
-jwt.verify(token,process.env.SECRET_KEY,(err,success)=>{
-    if(err){
-        res.status(400).json({
-            message: "Invalid token"
-        })
-    }else{
-        res.status(200).json({
-            message : "valid token"
-        })
-    }
-}) 
+// jwt.verify(token,process.env.SECRET_KEY,(err,success)=>{
+//     if(err){
+//         res.status(400).json({
+//             message: "Invalid token"
+//         })
+//     }else{
+//         res.status(200).json({
+//             message : "valid token"
+//         })
+//     }
+//}) 
+
+    // another method 
+
+    const decoded = await promisify(jwt.verify)(token,process.env.SECRET_KEY)
+
 
 }
 module.exports = isAuthenticated
