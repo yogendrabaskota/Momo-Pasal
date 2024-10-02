3c4bb493154e5159f174ffe7e45b504f9936aaa9
yogendrabaskota <yogendrabaskota18@gmail.com>
2024-10-02 12:35:34 +0545

    product create

--- middleware/isAuthenticated.js
+++ middleware/isAuthenticated.js
@@ -1,12 +1,12 @@
 const jwt = require("jsonwebtoken")
-const { promisify } = require("utils").promisify
+const {promisify} = require("util").promisify
 
 
 const isAuthenticated = async (req,res,next) => {
 const token = req.headers.authorization
 if(!token) {
     res.status(403).json({
-        message : "please provide token"
+        message : "please log in"
     })
 }
 //verify token is legit or not
@@ -24,8 +24,21 @@ if(!token) {
 
     // another method 
 
-    const decoded = await promisify(jwt.verify)(token,process.env.SECRET_KEY)
+const decoded = await promisify(jwt.verify)(token,process.env.SECRET_KEY)
+if(!decoded){
+    return res.status(403).json({
+        message : "do't try to do this"
+    })
+} 
+// check if user decoded.id(userid) exists in the table
+const doesUserExist = await UserActivation.findOne({_id : decoded.id})
+if(!doesUserExist){
+    return res.status(404).json({
+        message : "user doesn't exist with that token/id"
+    })
 
-
+}
+req.user = "Apple"
+    next()
 }
 module.exports = isAuthenticated
