2d3df1558c13ae49876e155456ac5566a1a1ba95
yogendrabaskota <yogendrabaskota18@gmail.com>
2024-10-03 23:23:38 +0545

    adminseeding

--- Database/database.js
+++ Database/database.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose")
 const User = require("../model/userModel")
-const adminSeeder = require("../asminSeeder")
+const adminSeeder = require("../adminSeeder")
 
 
 exports.connectDatabase = async()=>{
